7c30af7d2f254227f4b86aa27e86455acf35a29a
Thomas_Johnson <tjohnson1106@gmail.com>
2018-03-30 16:45:31 -0400

    added result card component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { ReactiveBase } from "@appbaseio/reactivesearch";
 import { DateRange } from "@appbaseio/reactivesearch";
 import { NumberBox } from "@appbaseio/reactivesearch";
 import { RangeSlider } from "@appbaseio/reactivesearch";
+import { ResultCard } from "@appbaseio/reactivesearch";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <section className="container">
+      <div className="container">
         <ReactiveBase
           app="housing"
           credentials="0aL1X5Vts:1ee67be1-9195-4f4b-bd4f-a91cd1b5e4b5"
           type="listing"
+          theme={{
+            primaryColor: "#FF3A4E"
+          }}
         >
           <DateRange
             dataField="date_from"
@@ -44,10 +48,46 @@ class App extends Component {
               start: "$10",
               end: "$250"
             }}
+            defaultSelected={{
+              start: 10,
+              end: 50
+            }}
+            stepValue={10}
+            react={{
+              and: ["DateRangeSensor"]
+            }}
           />
-          Reactive Search
+          <ResultCard
+            componentId="SearchResult"
+            dataField="name"
+            from={0}
+            size={10}
+            onData={data => ({
+              image: data.image,
+              title: data.name,
+              description: (
+                <div>
+                  <div className="price">${data.price}</div>
+                  <p className="info">
+                    {data.room_type} · {data.accommodates} guests
+                  </p>
+                </div>
+              ),
+              url: data.listing.url
+            })}
+            pagination
+            react={{
+              and: ["GuestSensor", "PriceSensor", "DateRangeSensor"]
+            }}
+            innerClass={{
+              resultStats: "result-stats",
+              list: "list",
+              listItem: "list-item",
+              image: "image"
+            }}
+          />
         </ReactiveBase>
-      </section>
+      </div>
     );
   }
 }
